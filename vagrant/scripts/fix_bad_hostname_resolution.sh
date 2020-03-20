8f15d8606e26a1b2cacf3d27c459a835201ac28d
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-03-20 03:34:22 +0100

    fix

--- vagrant/scripts/fix_bad_hostname_resolution.sh
+++ vagrant/scripts/fix_bad_hostname_resolution.sh
@@ -1,1 +1,10 @@
+source /vagrant/scripts/.env
+export NUM_NODES=$1
+export CURRENT_NODE=$2
+
 sudo perl -p -i -e 's/^127\.0\.1\.1.*$//g' /etc/hosts
+
+for ((i=1; i<=NUM_NODES; i++)); 
+do
+    sudo -- /bin/sh -c "echo '192.168.34.$((${i} + 100)) kafka-node-${i}' >> /etc/hosts"
+done
